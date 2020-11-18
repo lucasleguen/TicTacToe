8b95e33a3041ae4da56f83a80128dc00232ca2b0
lucasleguen <lucasleguen@hotmail.fr>
2020-11-18 17:30:40 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,9 @@
-def initialiseGrille (grille (9)):
+def initialiseGrille (grille[9]):
     for compteur in range (0,8):
         grille [compteur] = "_"
+return()
+
+def afficheGrille (grille[9]) :
+    for [i] in range (0,2) :
+        print(grille[3*i], grille[3*i+1], grille[3*i+2] : 
 return()
